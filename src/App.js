d11f67edc1dc32d84efc8c26b941dd3d6ee72646
tlhbyrz <talhaboyraz13@gmail.com>
2021-08-12 01:12:48 +0300

    header added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import HomeScreen from "./pages/Home"
 import NotFoundScreen from "./pages/Home"
+import Header from "./components/Header"
 
 function App() {
   return (
     <>
-      <Router>
-        <Switch>
-          <Route path='/' component={HomeScreen} exact />
-          <Route path='*' component={NotFoundScreen} />
-        </Switch>
-      </Router>
+      <Header />
+      <main className="container">
+        <Router>
+          <Switch>
+            <Route path='/' component={HomeScreen} exact />
+            <Route path='*' component={NotFoundScreen} />
+          </Switch>
+        </Router>
+      </main>
     </>
   );
 }
